be82201f120136500f9351541f29b307a7e1eb86
SmartVisionLights <67699101+SmartVisionLights@users.noreply.github.com>
2020-07-01 13:54:57 -0400

    Add files via upload

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,37 +1,111 @@
-String activate = "";
+#include <Wire.h>
+#include "SparkFun_VEML6030_Ambient_Light_Sensor.h"
+
+#define AL_ADDR 0x48
+
+SparkFun_Ambient_Light light(AL_ADDR);
+
+// Possible values: .125(1/8), .25(1/4), 1, 2
+// Both .125 and .25 should be used in most cases except darker rooms.
+// A gain of 2 should only be used if the sensor will be covered by a dark
+// glass.
+float gain = .125;
+
+// Possible integration times in milliseconds: 800, 400, 200, 100, 50, 25
+// Higher times give higher resolutions and should be used in darker light. 
+int time = 100;
+long luxVal = 0; 
+
+int delayVal = 800;
+
+int calibrationCycles = 20;
+
+String measure = "";
 String numOfReadings = "";
+String cellGain = "";
+String cellTime = "";
+String calibrate = "";
+
+double calibrationFactor;
 
 
 void setup() {
+  Wire.begin();
+  Serial.begin(256000); // the bigger number the better
+  light.begin();
+  cellGain = readCell("K2");
+  cellTime = readCell("L2");
+  gain = cellGain.toInt();
+  gain /= 1000.0;
 
-  Serial.begin(9600); // the bigger number the better
+  time = cellTime.toInt();
+  
+  // Again the gain and integration times determine the resolution of the lux
+  // value, and give different ranges of possible light readings. Check out
+  // hoookup guide for more info. 
+  light.setGain(gain);
+  light.setIntegTime(time);
+  float gainVal = light.readGain();
+  int timeVal = light.readIntegTime();
   Serial.println("RESETTIMER"); //resets timer to 0
 }
 
 void loop() {
-  activate = readCell("E2");
-  numOfReadings = readCell("F2");
+  // Monitor Activation Cell
+  measure = readCell("E2");
+  calibrate = readCell("G2");
   
-  if (activate.equals("Measuring")){
-    Serial.println("CLEARDATA"); //clears up any data left from previous projects
-    //writeCell("A1", "Values");
-    writeCell("E2", "Measuring");
-    writeCell("F2", String(numOfReadings));
+  if (measure.equals("Measuring")){
+    numOfReadings = readCell("F2");
+    
     for (int i = 0; i < 100; i++) {
       String location = "A";
       location += (2 + i);
       if (i > numOfReadings.toInt()) {
         writeCell(location,"");
       }else{
-        writeCell(location, String(random(0,100)));
+        luxVal = light.readLight();
+        writeCell(location, String(luxVal-calibrationFactor));
+        delay(delayVal);
       }
-      delay(10);
     }
     writeCell("E2", "Measure");
   }
 
-  delay(100);
+  if (calibrate.equals("Calibrating")) {
+    calibrationFactor = calibrateSensor();
+    writeCell("H2", String(calibrationFactor));
+    writeCell("G2", String("Calibrate"));
+  }
+  
 }
+
+double calibrateSensor() {
+  cellGain = readCell("K2");
+  cellTime = readCell("L2");
+  
+  gain = cellGain.toInt();
+  gain /= 1000.0;
+  
+  time = cellTime.toInt();
+
+  light.setGain(gain);
+  light.setIntegTime(time);
+  
+  int measurements[calibrationCycles];
+
+  double total = 0;
+  
+  for (int i = 0; i < calibrationCycles; i++) {
+    luxVal = light.readLight();
+    measurements[i] = luxVal;
+    total += luxVal;
+  }
+
+  double average = total/calibrationCycles;
+  return average;
+}
+
 
 void writeCell(String cell, String value) {
   Serial.print("CELL,SET," + cell + ",");
@@ -40,10 +114,10 @@ void writeCell(String cell, String value) {
 
 String readCell(String cell) {
   Serial.println("CELL,GET," + cell + ",");
-  delay(100);
   String cellData = "";
+  delay(50);
   if (Serial.available() > 0) {
-    cellData = Serial.readStringUntil('.');
+    cellData = Serial.readString();
     cellData.trim();
   }
   return cellData;
